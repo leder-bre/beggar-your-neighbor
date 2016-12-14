165bb57e99dcc6370d1096c38b5a88f7fe80527f
rsgc-leder-b <bleder@rsgc.on.ca>
2016-12-14 13:41:55 -0500

    Initial Commit

---
+++ beggar-your-neighbor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  beggar-your-neighbor
+//
+//  Created by Brendan Leder on 2016-12-14.
+//  Copyright © 2016 Brendan Leder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
